c8640cf646eadf509eb0e9189c1d34c0357823a1
anthonyb0824 <anthonybutler0824@gmail.com>
2022-05-07 07:37:06 -0500

    filled out tradeServiceImpl

--- StockCentralREST/src/main/java/com/skilldistillery/services/TradeService.java
+++ StockCentralREST/src/main/java/com/skilldistillery/services/TradeService.java
@@ -6,6 +6,10 @@ import com.skilldistillery.entities.Trade;
 
 public interface TradeService {
 		List<Trade> index();
+		Trade findById(int id);
+		Trade createTrade(Trade newtrade);
+		Trade updateTrade(Trade newTrade);
+		Void deleteTrade(int id);
 	}
 
 
--- StockCentralREST/src/main/java/com/skilldistillery/services/TradeServiceImpl.java
+++ StockCentralREST/src/main/java/com/skilldistillery/services/TradeServiceImpl.java
@@ -19,4 +19,25 @@ public class TradeServiceImpl implements TradeService {
 		return tradeRepo.findAll();
 	}
 
+	@Override
+	public Trade findById(int id) {
+		return tradeRepo.getById(id);
+	}
+
+	@Override
+	public Trade createTrade(Trade newtrade) {
+		return tradeRepo.save(newtrade);
+	}
+
+	@Override
+	public Trade updateTrade(Trade newTrade) {
+		return tradeRepo.saveAndFlush(newTrade);
+	}
+
+	@Override
+	public Void deleteTrade(int tradeId) {
+		 tradeRepo.deleteById(tradeId);
+		return null;
+	}
+
 }
